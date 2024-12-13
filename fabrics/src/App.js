ef0c282dad681b0f80be6b29f34a4c9558f70796
fayrouz41 <FS2200869@tkh.edu.eg>
2024-12-13 22:06:28 +0200

    ....

--- fabrics/src/App.js
+++ fabrics/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import RegistrationPage from './pages/register';
@@ -5,7 +7,7 @@ import RegistrationPage from './pages/register';
 function App() {
   return (
     <div className="App">
-      
+      <RegistrationPage/>
     </div>
   );
 }
